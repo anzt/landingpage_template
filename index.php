474ff02163a635f65d2e6a03c5b81e42396ef09e
Anas Syaifudin <anzt.07@gmail.com>
2020-07-10 15:21:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Services Free Template by Colorlib</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+	<link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/jquery-ui.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+	<link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
+
+	<link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+
+	<link rel="stylesheet" href="assets/fonts/flaticon/font/flaticon.css">
+
+	<link rel="stylesheet" href="assets/css/aos.css">
+
+	<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+	<div id="overlayer"></div>
+	<div class="loader">
+		<div class="spinner-border text-primary" role="status">
+			<span class="sr-only">Loading...</span>
+		</div>
+	</div>
+
+
+	<div class="site-wrap">
+
+		<div class="site-mobile-menu site-navbar-target">
+			<div class="site-mobile-menu-header">
+				<div class="site-mobile-menu-close mt-3">
+					<span class="icon-close2 js-menu-toggle"></span>
+				</div>
+			</div>
+			<div class="site-mobile-menu-body"></div>
+		</div>
+
+		<?php include('header.php') ?>
+
+		<!-- Banner -->
+		<div class="site-blocks-cover overlay bg-light" style="background-image: url('assets/images/hero_bg_1.jpg'); " id="home-section">
+
+			<div class="container">
+				<div class="row justify-content-center">
+
+					<div class="col-12 text-center align-self-center text-intro">
+						<div class="row justify-content-center">
+							<div class="col-lg-8">
+								<h1 class="text-white" data-aos="fade-up" data-aos-delay="">We Are Digital Services</h1>
+								<p class="lead text-white" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos magnam maxime voluptates libero, nobis impedit aut corrupti sunt possimus.</p>
+								<p data-aos="fade-up" data-aos-delay="200"><a href="#services-section" class="btn smoothscroll btn-primary">Our Services</a></p>
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		</div> 
+		<!-- End of banner -->
+		<?php include('footer.php') ?>
+	</div> <!-- .site-wrap -->
+
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/jquery-ui.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jquery.easing.1.3.js"></script>
+	<script src="assets/js/aos.js"></script>
+	<script src="assets/js/jquery.fancybox.min.js"></script>
+	<script src="assets/js/jquery.sticky.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+
+
+	<script src="assets/js/main.js"></script>
+</body>
+</html>
